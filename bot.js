9cd96de2418550ae5ae3cd9e2b5356924a4e772b
Dhruv Prakash <dhruvprakash@Dhruvs-Air.wv.cc.cmu.edu>
2017-05-05 17:18:57 -0400

    download an image from memecenter and tweet it out

--- bot.js
+++ bot.js
@@ -1,79 +1,75 @@
 console.log('zoidberg');
-
+var Xray = require('x-ray');
 var Twit = require('twit');
 var fs = require('fs');
-//authenticate with oauth
+var request = require('request');
 var config = require('./config');
-
 var T = new Twit(config);
 
-//get() -> search by hashtag, location, user -> give me all the tweets about pokemon
-//post() -> tweeting
-//stream() -> ??? continuous connection like a socket! anytime someone @mentions me on twitter this stream() triggers an event
-// for example: every time trump tweets trigger an event make up an apology and post it :D
+var xray = new Xray();
 
+var download = function(uri, filename, callback){
+  console.log('download called');
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
 
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
 
-// var params = {
-// 	q: 'attack on titan',
-// 	count: 2,
-// 	lang: 'en'
-// };
+xray('https://www.memecenter.com/search?query=anime','img.rrcont',
+  [{
+    src: '@src'
+  }])(function(err, dataArr) {
+  	var rand = Math.floor(Math.random() * dataArr.length);
+  	console.log(dataArr[rand]);
 
-// var tweet = {
-// 	status: 'You thought this was an example.. but it was me DIO!!!!!!!'
-// };
+  	download(dataArr[rand]['src'], './images/funny.png', function(){
+  	  var b64content = fs.readFileSync('./images/funny.png', {encoding: 'base64'});
 
-// var gotData = function(err, data, response) {
-// 	var tweets = data.statuses;
-// 	tweets.forEach(function(tweet){
-// 		console.log(tweet.text);
-// 	})
-// };
+  	  // // //console.log(b64content);
 
-// T.get('search/tweets', params, gotData);
+  	  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+  	    // now we can assign alt text to the media, for use by screen readers and
+  	    // other text-based presentations and interpreters
+  	    var mediaIdStr = data.media_id_string;
+  	    var altText = 'here take this:';
+  	    var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
 
-// var tweeted = function(err,data,response){
-// 	if(err) {
-// 		console.log('something went wrong');
-// 		console.log(err);
-// 	} else {
-// 		console.log('WRYYYYYY');
-// 	}
-// }
+  	    if(err) {
+  	    	console.log('error in the post :/');
+  	    	console.log(err);
+  	    }
 
-// T.post('statuses/update',tweet, tweeted);
+  	    T.post('media/metadata/create', meta_params, function (err, data, response) {
+  	      if (!err) {
+  	        // now we can reference the media and post a tweet (media will attach to the tweet)
+  	        var params = { status: 'Take this meme and be happy :)', media_ids: [mediaIdStr] }
 
+  	        T.post('statuses/update', params, function (err, data, response) {
+  	          console.log(data)
+  	        })
+  	      } else {
+  	      	console.log(err);
+  	      }
+  	    })
+  	  })
 
-var b64content = fs.readFileSync('./images/shitbag-josuke-idiot-okuyasu.png', {encoding: 'base64'});
+  	  console.log('done');
+  	});
 
-//console.log(b64content);
-
-T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-  // now we can assign alt text to the media, for use by screen readers and
-  // other text-based presentations and interpreters
-  var mediaIdStr = data.media_id_string;
-  var altText = 'shitbag josuke and idiot okuyasu';
-  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
-
-  if(err) {
-  	console.log('error in the first post :/');
-  	console.log(err);
-  }
-
-  T.post('media/metadata/create', meta_params, function (err, data, response) {
-    if (!err) {
-      // now we can reference the media and post a tweet (media will attach to the tweet)
-      var params = { status: 'Rohan sensei\'s thoughts - #JOJO #diamond #oraoraora #heavensdoor', media_ids: [mediaIdStr] }
-
-      T.post('statuses/update', params, function (err, data, response) {
-        console.log(data)
-      })
-    } else {
-    	console.log(err);
-    }
   })
-})
+
+//authenticate with oauth
+// //get() -> search by hashtag, location, user -> give me all the tweets about pokemon
+// //post() -> tweeting
+// //stream() -> ??? continuous connection like a socket! anytime someone @mentions me on twitter this stream() triggers an event
+// // for example: every time trump tweets trigger an event make up an apology and post it :D
+
+
+
+
 
 
 
